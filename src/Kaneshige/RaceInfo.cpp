604fa9ffd7d268885a3b037bfc872b5835433992
SwareJonge <olaf23okken@gmail.com>
2022-06-07 16:23:35 +0200

    move files to proper folder and set stuff up for recompiling

---
+++ src/Kaneshige/RaceInfo.cpp
@@ -0,0 +1,101 @@
+#include "RaceInfo.h"
+
+void RaceInfo::reset() {
+    isTinyProcess = false;
+    isLanMode = false;
+    isTrueEnding = false;
+    randomSeed = 0;
+    gpCup = -1;
+    raceMode = 0;
+    raceLevel = -1;
+    itemSlotType = 0;
+    vsLapNum = 0;
+    lapNumLAN = 0;
+    kartNum = 0;
+    playerNum = 0;
+    consoleNum = 0;
+    statusNum = 0;
+    lod = 0;
+    gpStageNo = 0;
+    _0x28 = 0;
+    isMirror = false;
+    _0x298 = 0;
+    hideConsole = 0;
+
+    for (s32 i = 0; i < 8; i++) {
+        kartInfo[i].reset();
+        rank[i] = 0;
+    }
+
+    for (s32 i = 0; i < 8; i++) {
+        startPosIndex[i] = i;
+        pointTable[i] = 0;
+    }
+
+    for (s32 i = 0; i < 2; i++) {
+        rivalKarts[i] = -1;
+    }
+
+    for (s32 i = 0; i < 4; i++) {
+        _0x114[i] = i;
+        _0x11c[i] = 0;
+    }
+
+    awardKartNo = -1;
+    demoNextPhase = 6;
+}
+
+void RaceInfo::setConsoleTarget(s32 idx, s16 p2, bool p3) {
+    _0x114[idx] = p2;
+    _0x11c[idx] = p3;
+}
+
+void RaceInfo::setRace(ERaceMode RaceMode, s32 kartCount, s32 playerCount, s32 consoleCount, s32 p5) {
+    reset();
+    raceMode = RaceMode;
+    kartNum = (s16)kartCount;
+    playerNum = playerCount;
+    consoleNum = (s16)consoleCount;
+    statusNum = (s16)p5;
+    lod = 0;
+
+    if (consoleNum >= 3)
+        lod |= 2;
+
+    if ((consoleCount >= 3) && (kartCount >= 5))
+        isTinyProcess = true;
+}
+
+void RaceInfo::setRaceLevel(ERaceLevel raceLvl) {
+    raceLevel = raceLvl;
+    if (raceLvl == LVL_MIRROR)
+        isMirror = true;
+    else
+        isMirror = false;
+}
+
+void RaceInfo::shuffleRandomSeed() {
+    // maybe randomSeed is TRandom_fast_? uses a template function in the debug version, probably inlined here
+    JMath::TRandom_fast_ rndm(randomSeed);
+    randomSeed = rndm.get();
+}
+
+void RaceInfo::shuffleStartNo() {
+    JMath::TRandom_fast_ rndm(randomSeed);
+    u32 seed;
+
+    for (u32 i = 0; i < (u32)getKartNumber(); i++) {
+        s32 kartIdx = getKartNumber();
+        u32 seed = rndm.get();
+
+        u32 newidx = i + (seed % (kartIdx - i));
+        s32 playerStartIdx = startPosIndex[i];
+        startPosIndex[i] = startPosIndex[newidx];
+        startPosIndex[newidx] = playerStartIdx;
+    }
+
+}
+
+void RaceInfo::hideConsole(u32 param_2) {
+    hideConsole = hideConsole | (u16)(1 << param_2);
+}
--- src/Kaneshige/RaceInfo.h
+++ src/Kaneshige/RaceInfo.h
@@ -7,18 +7,57 @@
 #include "KartInfo.h"
 #include "RaceTime.h"
 
+enum ERaceMode {
+    TIME_ATTACK = 0x1,
+    GRAND_PRIX = 0x2,
+    VERSUS_RACE = 0x3,
+    BALLOON_BATTLE = 0x4,
+    ROBBERY_BATTLE =	0x5,
+    BOMB_BATTLE =	0x6,
+    ESCAPE_BATTLE =	0x7,
+    AWARD_DEMO	= 0x8,
+    STAFF_ROLL =	0x9,
+};
+
+enum ERaceLevel { // unsure of this
+    LVL_INV = -1,
+    LVL_50CC = 0,
+    LVL_100CC = 1,
+    LVL_150CC = 2,
+    LVL_MIRROR = 3,
+};
+
+enum ERaceGpCup {
+    MUSHROOM_CUP = 0,
+    FLOWER_CUP = 1,
+    STAR_CUP = 2,
+    SPECIAL_CUP = 3,
+    REVERSE2_CUP = 4,
+};
+
 class RaceInfo {
 public:
     inline RaceInfo(); // this is not inlined in the debug version 
     ~RaceInfo();
 
     void reset();
+    void setConsoleTarget(s32 idx, s16 p2, bool p3);
+    void setRace(ERaceMode RaceMode, s32 kartCount, s32 playerCount, s32 consoleCount, s32 p5);
+    void setRaceLevel(ERaceLevel);
+
+    void shuffleRandomSeed();
+    void shuffleStartNo();
+    void hideConsole(u32 param_2);
+
+    s16 getKartNumber() {
+        return kartNum;
+    }
 
 private:
     bool isTinyProcess;
     bool isLanMode;
     bool isTrueEnding;
-    JMath::TRandom_fast_ random;
+    u32 randomSeed;
     ERaceMode raceMode;
     ERaceGpCup gpCup;
     ERaceLevel raceLevel;
@@ -29,17 +68,19 @@ private:
     s16 playerNum;
     s16 consoleNum;
     s16 statusNum;
-    s16 lod;
+    u16 lod;
     s16 gpStageNo;
-    s32 _0x28; 
+    s32 _0x28;
     bool isMirror;
     // padding bytes
     KartInfo kartInfo[8];
     s16 startPosIndex[8];
     s16 pointTable[8];
     s16 rivalKarts[2];
+    
     s16 _0x114[4];
-    u8 _0x11c[4]; // not sure what these two arrays do
+    bool _0x11c[4]; // not sure what these two arrays do, setConsoleTarget sets this so maybe this bool array means isActive and the other the console number
+
     s16 awardKartNo;
     // padding bytes
     s32 demoNextPhase;
