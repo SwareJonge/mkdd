e7a42eeaee99984cdfaf121c98af77cd967dd240
SwareJonge <olaf23okken@gmail.com>
2022-07-11 23:04:01 +0200

    Decompile RaceInfo.cpp

--- include/JSystem/JUtility/JUTAssert.h
+++ include/JSystem/JUtility/JUTAssert.h
@@ -7,6 +7,9 @@ class JUTAssertion {
 public:
     static u32 getSDevice(void);
     static void showAssert_f(u32 device, char const * file, int line, char const * errormsg, ...);
+    static void showAssert(u32 device, char const *file, int line, char const *errormsg); /* {
+      showAssert_f(device, file, line, "%s", errormsg);
+  }*/
 };
 
 #endif
