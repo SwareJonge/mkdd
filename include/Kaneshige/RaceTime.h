600a30a8b4fd39f43e4823645be73acf13180ce7
SwareJonge <olaf23okken@gmail.com>
2022-07-12 13:27:43 +0200

    build RaceTime.cpp

--- include/Kaneshige/RaceTime.h
+++ include/Kaneshige/RaceTime.h
@@ -3,30 +3,73 @@
 
 #include "types.h"
 
+#define MAX_TIME 5999999
+#define MAX_MS 999
+
 class RaceTime
 {
 public:
-    inline RaceTime();
-    //inline RaceTime(RaceTime const &);
+    RaceTime() {
+        reset();
+    }
+    // this constructor might not be needed
+    /*RaceTime::RaceTime(RaceTime const & racetime) {
+        value = racetime.value;
+    }*/
+
     //inline void operator=(const RaceTime &);
-    inline void reset();
-    inline void zero();
-    inline void set(int);
-    inline void set(RaceTime const &);
-    inline bool isAvailable();
-    inline bool isLittle(const RaceTime &) const;
-
-    inline void sub(const RaceTime &, const RaceTime &);
-    inline int get() const;
-    inline void sub(int p1);
-
-    inline void setFrame(int);
-    inline int getUpwardMSec() const;
-
     void get(int *, int *, int *) const;
 
+    int get() const {
+        return value;
+    }
+
+    int getUpwardMSec() const {
+        return value + MAX_MS;
+    }
+
+    bool isAvailable() {
+        return value != MAX_TIME;
+    }
+
+    bool isLittle(RaceTime const & raceTime) const {
+        return value < raceTime.value;
+    }
+
+    void reset() {
+        value = MAX_TIME;
+    }
+
+    void set(int frame) {
+        value = frame; // no idea why framecount is used here, 
+        if (value > MAX_TIME)
+            value = MAX_TIME;
+    }
+
+    /*void set(RaceTime const & raceTime) {
+        *this = raceTime;
+    }*/
+
+    void setFrame(int frame) {
+        value = (frame * 1000) / 60;
+        if (value > MAX_TIME)
+            value = MAX_TIME;
+    }
+
+    void sub(const RaceTime & split1, const RaceTime & split2) {
+        value = split1.value - split2.value;
+    }
+
+    void sub(int p1) {
+        value = value - p1;
+    }
+
+    void zero() {
+        value = 0;
+    }
+
 private:
-    int time;
+    int value;
 };
 
 #endif
--- src/Kaneshige/RaceTime.cpp
+++ src/Kaneshige/RaceTime.cpp
@@ -1,23 +1,7 @@
 #include "Kaneshige/RaceTime.h"
 
-#define MAX_TIME 5999999
-#define MAX_MS 999
-
-RaceTime::RaceTime() {
-    reset();
-}
-
-// this constructor might not be needed
-/*RaceTime::RaceTime(RaceTime const & racetime) {
-    time = racetime.time;
-}*/
-
-int RaceTime::get() const {
-    return time;
-}
-
 void RaceTime::get(int *minutes, int * seconds, int *miliseconds) const {
-    int _time = time;
+    int _time = value;
     if (_time < 0) {
         _time = -_time;
     }
@@ -26,51 +10,7 @@ void RaceTime::get(int *minutes, int * seconds, int *miliseconds) const {
     *seconds = (_time / 1000) % 60;
     *minutes = (_time / 1000) / 60;
 
-    if (time < 0)
+    if (value < 0)
         *minutes = -*minutes;
 
-}
-
-int RaceTime::getUpwardMSec() const {
-    return time + MAX_MS;
-}
-
-void RaceTime::set(int frame) {
-    time = frame; // no idea why framecount is used here, 
-    if (time > MAX_TIME)
-        time = MAX_TIME;
-}
-
-void RaceTime::set(RaceTime const & raceTime) {
-    *this = raceTime;
-}
-
-void RaceTime::reset() {
-    time = MAX_TIME;
-}
-
-void RaceTime::setFrame(int frame) {
-    time = (frame * 1000) / 60;
-    if (time > MAX_TIME)
-        time = MAX_TIME;
-}
-
-bool RaceTime::isAvailable() {
-    return time != MAX_TIME;
-}
-
-bool RaceTime::isLittle(RaceTime const & raceTime) const {
-    return time < raceTime.time;
-}
-
-void RaceTime::sub(const RaceTime & split1, const RaceTime & split2) {
-    time = split1.time - split2.time;
-}
-
-void RaceTime::sub(int p1) {
-    time = time - p1;
-}
-
-void RaceTime::zero() {
-    time = 0;
 }
