900b123f52f94c2e2dc32714836e8a0f10ac5991
SwareJonge <olaf23okken@gmail.com>
2023-01-17 00:31:24 +0100

    add JFWSystem Inlines for System::init

--- include/JSystem/JFrameWork/JFWSystem.h
+++ include/JSystem/JFrameWork/JFWSystem.h
@@ -2,16 +2,15 @@
 #define _JSYSTEM_JFW_JFWSYSTEM_H
 
 #include "dolphin/gx.h"
+#include "JSystem/JKernel/JKRHeap.h"
+#include "JSystem/JKernel/JKRThread.h"
+#include "JSystem/JUtility/JUTAssert.h"
+#include "JSystem/JUtility/JUTConsole.h"
+#include "JSystem/JUtility/JUTDbPrint.h"
 #include "JSystem/JUtility/JUTFont.h"
 #include "types.h"
 
-struct JKRThread;
-struct JKRHeap;
-struct JUTConsole;
-struct JUTConsoleManager;
-struct JUTDbPrint;
-struct JUTFont;
-
+// Maybe these are namespaces?
 struct JFWSystem
 {
     struct CSetUpParam
@@ -40,6 +39,31 @@ struct JFWSystem
     static JUTConsoleManager *systemConsoleManager;
     static JUTConsole *systemConsole;
     static bool sInitCalled;
+
+
+    
+    // No idea how they formatted this lol
+    static void setMaxStdHeaps(u32 stdHeaps) { 
+        JUT_ASSERT(47, sInitCalled == 0);
+        CSetUpParam::maxStdHeaps = stdHeaps; }
+    static void setSysHeapSize(u32 heapSize) {
+        JUT_ASSERT(50, sInitCalled == 0);
+        CSetUpParam::sysHeapSize = heapSize; }
+    static void setFifoBufSize(u32 bufSize) {
+        JUT_ASSERT(53, sInitCalled == 0);
+        CSetUpParam::fifoBufSize = bufSize; }
+    
+    // Inlines for Aram
+    static void setAramAudioBufSize(u32 bufSize) {
+        JUT_ASSERT(58, sInitCalled == 0);
+        CSetUpParam::aramAudioBufSize = bufSize; }
+    static void setAramGraphBufSize(u32 bufSize) {
+        JUT_ASSERT(61, sInitCalled == 0);
+        CSetUpParam::aramGraphBufSize = bufSize; }
+    // probably some more inlines for other variables, not used by MKDD
+    static void setRenderMode(GXRenderModeObj * rmode) {
+        JUT_ASSERT(80, sInitCalled == 0);
+        CSetUpParam::renderMode = rmode; }
 };
 
 #endif
