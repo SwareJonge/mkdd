ae7231c2f5f495ac67e7509a28bfeb42b922330f
Andrés Sánchez <55501523+andresj-sanchez@users.noreply.github.com>
2025-10-04 20:53:32 -0400

    Remove unnecessary padding bytes

--- include/Yamamoto/KartCheck.h
+++ include/Yamamoto/KartCheck.h
@@ -27,7 +27,6 @@ public:
 private:
     KartBody* mBody;    // 0x0 (Inferred from assembly: stw this, 0x0(r30))
     u8 mField4;         // 0x4 (Inferred from assembly: stb r0, 0x4(r30), where r0=0)
-    u8 _5[0x3];         // 0x5-0x7 - Padding to reach size 0x8
 
 }; // 0x8
 
