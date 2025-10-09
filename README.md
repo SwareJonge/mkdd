587331adf112813627b1bab5deab12e58969c299
Luke Street <luke@street.dev>
2025-10-09 10:17:49 -0600

    Update dtk-template, remove ppcdis & migrate to doldecomp

--- README.md
+++ README.md
@@ -1,33 +1,100 @@
 Mario Kart Double Dash!!  
-[![Build Status]][actions] [![Code Progress]][progress]
+[![Build Status]][actions] [![Code Progress]][progress] [![Data Progress]][progress] [![Discord Badge]][discord]
 =============
 
-[Build Status]: https://github.com/SwareJonge/mkdd/actions/workflows/build.yml/badge.svg
-[actions]: https://github.com/SwareJonge/mkdd/actions/workflows/build.yml
-[Code Progress]: https://decomp.dev/SwareJonge/mkdd.svg?mode=shield&measure=code&label=Code
-[progress]: https://decomp.dev/SwareJonge/mkdd
+[Build Status]: https://github.com/doldecomp/mkdd/actions/workflows/build.yml/badge.svg
+[actions]: https://github.com/doldecomp/mkdd/actions/workflows/build.yml
+[Code Progress]: https://decomp.dev/doldecomp/mkdd.svg?mode=shield&measure=code&label=Code
+[Data Progress]: https://decomp.dev/doldecomp/mkdd.svg?mode=shield&measure=data&label=Data
+[progress]: https://decomp.dev/doldecomp/mkdd
+[Discord Badge]: https://img.shields.io/discord/727908905392275526?color=%237289DA&logo=discord&logoColor=%23FFFFFF
+[discord]: https://discord.gg/hKx3FJJgrV
 
 WIP 1:1 Decompilation of Mario Kart Double Dash!!
 
 This repository does **not** contain any game assets or assembly whatsoever. An existing copy of the game is required.
 
-Currently only the debug version is supported
+Currently only the debug version is supported.
 
-## Building
-### Requirements
-- [Ninja](https://github.com/ninja-build/ninja/releases/tag/v1.11.1)
-- [Python 3.8+](https://www.python.org/downloads/release/python-3104/) 
+Dependencies
+============
 
-### Initial setup
-- Install python, 3.10.4 is recommended as it gets installed by default on most recent Linux distros
-- Add the path where ninja is located to your environment variables under `PATH`
-- Clone the repository using `git clone https://github.com/SwareJonge/mkdd`
-- Dump a copy of the Debug version and copy the disc image to `orig/MarioClub_us`
-- After the initial build, the disc image can be removed to save space
+Windows
+--------
 
-### Instructions
-- run `python configure.py`
-- run `ninja`
+On Windows, it's **highly recommended** to use native tooling. WSL or msys2 are **not** required.  
+When running under WSL, [objdiff](#diffing) is unable to get filesystem notifications for automatic rebuilds.
+
+- Install [Python](https://www.python.org/downloads/) and add it to `%PATH%`.
+  - Also available from the [Windows Store](https://apps.microsoft.com/store/detail/python-311/9NRWMJP3717K).
+- Download [ninja](https://github.com/ninja-build/ninja/releases) and add it to `%PATH%`.
+  - Quick install via pip: `pip install ninja`
+
+macOS
+------
+
+- Install [ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages):
+
+  ```sh
+  brew install ninja
+  ```
+
+- Install [wine-crossover](https://github.com/Gcenx/homebrew-wine):
+
+  ```sh
+  brew install --cask --no-quarantine gcenx/wine/wine-crossover
+  ```
+
+After OS upgrades, if macOS complains about `Wine Crossover.app` being unverified, you can unquarantine it using:
+
+```sh
+sudo xattr -rd com.apple.quarantine '/Applications/Wine Crossover.app'
+```
+
+Linux
+------
+
+- Install [ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages).
+- For non-x86(_64) platforms: Install wine from your package manager.
+  - For x86(_64), [wibo](https://github.com/decompals/wibo), a minimal 32-bit Windows binary wrapper, will be automatically downloaded and used.
+
+Building
+========
+
+- Clone the repository:
+
+  ```sh
+  git clone https://github.com/doldecomp/mkdd.git
+  ```
+
+- Copy your game's disc image to `orig/MarioClub_us`.
+  - Supported formats: ISO (GCM), RVZ, WIA, WBFS, CISO, NFS, GCZ, TGC
+  - After the initial build, the disc image can be deleted to save space.
+
+- Configure:
+
+  ```sh
+  python configure.py
+  ```
+
+  To use a version other than `MarioClub_us` (USA), specify it with `--version`.
+
+- Build:
+
+  ```sh
+  ninja
+  ```
+
+Diffing
+=======
+
+Once the initial build succeeds, an `objdiff.json` should exist in the project root.
+
+Download the latest release from [encounter/objdiff](https://github.com/encounter/objdiff). Under project settings, set `Project directory`. The configuration should be loaded automatically.
+
+Select an object from the left sidebar to begin diffing. Changes to the project will rebuild automatically: changes to source files, headers, `configure.py`, `splits.txt` or `symbols.txt`.
+
+![](assets/objdiff.png)
 
 ## Credits
 - [Seeky](https://github.com/SeekyCt) for ppcdis, without it this project wouldn't have been where it is now
--- configure.py
+++ configure.py
@@ -104,6 +104,12 @@ parser.add_argument(
     help="path to sjiswrap.exe (optional)",
 )
 parser.add_argument(
+    "--ninja",
+    metavar="BINARY",
+    type=Path,
+    help="path to ninja binary (optional)",
+)
+parser.add_argument(
     "--verbose",
     action="store_true",
     help="print verbose output",
@@ -113,6 +119,13 @@ parser.add_argument(
     dest="non_matching",
     action="store_true",
     help="builds equivalent (but non-matching) or modded objects",
+)
+parser.add_argument(
+    "--warn",
+    dest="warn",
+    type=str,
+    choices=["all", "off", "error"],
+    help="how to handle warnings",
 )
 parser.add_argument(
     "--no-progress",
@@ -139,6 +152,7 @@ config.compilers_path = args.compilers
 config.generate_map = args.map
 config.non_matching = args.non_matching
 config.sjiswrap_path = args.sjiswrap
+config.ninja_path = args.ninja
 config.progress = args.progress
 if not is_windows():
     config.wrapper = args.wrapper
@@ -151,21 +165,21 @@ config.shift_jis = False
 
 # Tool versions
 config.binutils_tag = "2.42-1"
-config.compilers_tag = "20240706"
-config.dtk_tag = "v1.1.4"
-config.objdiff_tag = "v2.3.2"
-config.sjiswrap_tag = "v1.1.1"
-config.wibo_tag = "0.6.11"
+config.compilers_tag = "20250812"
+config.dtk_tag = "v1.6.2"
+config.objdiff_tag = "v3.3.1"
+config.sjiswrap_tag = "v1.2.1"
+config.wibo_tag = "0.7.0"
 
 # Project
-config.config_path = Path("config") / config.version / "config_dtk.yml"
+config.config_path = Path("config") / config.version / "config.yml"
 config.check_sha_path = Path("config") / config.version / "build.sha1"
 config.asflags = [
     "-mgekko",
     "--strip-local-absolute",
     "-I include",
     f"-I build/{config.version}/include",
-    f"--defsym version={version_num}",
+    f"--defsym BUILD_VERSION={version_num}",
 ]
 config.ldflags = [
     "-fp hardware",
@@ -178,6 +192,10 @@ if args.map:
 
 # Use for any additional files that should cause a re-configure when modified
 config.reconfig_deps = []
+
+# Optional numeric ID for decomp.me preset
+# Can be overridden in libraries or objects
+config.scratch_preset_id = 66  # Mario Kart: Double Dash!!
 
 cflags_base = [
     "-nodefaults",
@@ -199,18 +217,28 @@ cflags_base = [
     "-multibyte",  # For Wii compilers, replace with `-enc SJIS`
     "-i include",
     "-i libs/PowerPC_EABI_Support/include",
-    f"-i build/{config.version}/include"
+    f"-i build/{config.version}/include",
+    f"-DBUILD_VERSION={version_num}",
+    f"-DVERSION_{config.version}",
 ]
 
 if "_eu" in config.version:
-    cflags_base.extend(["-DVIDEO_PAL=1", "-DREGION_EU=1",])
+    cflags_base.extend(["-DVIDEO_PAL=1", "-DREGION_EU=1"])
 elif "_us" in config.version:
-    cflags_base.extend(["-DREGION_US=1",])
+    cflags_base.extend(["-DREGION_US=1"])
 elif "_jp" in config.version:
-    cflags_base.extend(["-DREGION_JP=1",])
+    cflags_base.extend(["-DREGION_JP=1"])
 
 if not config.non_matching:
     cflags_base.extend(["-DMATCHING=1"])
+
+# Warning flags
+if args.warn == "all":
+    cflags_base.append("-W all")
+elif args.warn == "off":
+    cflags_base.append("-W off")
+elif args.warn == "error":
+    cflags_base.append("-W error")
 
 # Metrowerks library flags
 cflags_runtime = [
@@ -1444,6 +1472,25 @@ config.libs = [
     ),
 ]
 
+
+# Optional callback to adjust link order. This can be used to add, remove, or reorder objects.
+# This is called once per module, with the module ID and the current link order.
+#
+# For example, this adds "dummy.c" to the end of the DOL link order if configured with --non-matching.
+# "dummy.c" *must* be configured as a Matching (or Equivalent) object in order to be linked.
+def link_order_callback(module_id: int, objects: List[str]) -> List[str]:
+    # Don't modify the link order for matching builds
+    if not config.non_matching:
+        return objects
+    if module_id == 0:  # DOL
+        return objects + ["dummy.c"]
+    return objects
+
+
+# Uncomment to enable the link order callback.
+# config.link_order_callback = link_order_callback
+
+
 # Optional extra categories for progress tracking
 config.progress_categories = [
     ProgressCategory("game", "Game Code"),
@@ -1452,6 +1499,12 @@ config.progress_categories = [
     ProgressCategory("msl", "MSL"),
 ]
 config.progress_each_module = args.verbose
+# Optional extra arguments to `objdiff-cli report generate`
+config.progress_report_args = [
+    # Marks relocations as mismatching if the target value is different
+    # Default is "functionRelocDiffs=none", which is most lenient
+    # "--config functionRelocDiffs=data_value",
+]
 
 if args.mode == "configure":
     # Write build.ninja and objdiff.json
